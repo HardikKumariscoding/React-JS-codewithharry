a6719ae8e8dc46f686e64f1b0fe5eb5d236ac257
Hardik kumar <hardikgugnani123@gmail.com>
2024-12-09 00:07:28 -0500

    Video-8: adding more logic to textutils

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -9,20 +9,48 @@ export default function TextForm(props) {
          setText(newText);
     }
 
+    const handleLoClick = () =>{
+        let newText = text.toLowerCase();
+
+
+         setText(newText);
+    }
+
     const handleOnChange = (event) =>{
         setText(event.target.value)
         // setText("You just triggered handleUpClick")
     }
 
+    const RemoveText = () =>{
+        setText("");
 
-    const [text,setText] = useState('Enter text here bruh');
+    }
+
+
+    const [text,setText] = useState('');
     return (
+        <>
         <div>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" id="myBox" rows="8" onChange={handleOnChange}  value={text}></textarea>
             </div>
-            <button className='btn btn-primary' onClick={handleUpClick} >Convert to Uppercase</button>
+            <button className='btn btn-primary mx-2' onClick={handleUpClick} >Convert to Uppercase</button>
+            <button className='btn btn-primary mx-2' onClick={handleLoClick} >Convert to Lowecase</button>
+            <button className='btn btn-primary mx-2' onClick={RemoveText} >Remove all</button>
         </div>
+
+        <div className='container my-8'>
+            <h1>Your text summary</h1>
+            <p>  {text.split(" ").length} words and {text.length} characters</p>
+            <p> Its a {0.008 * text.split(" ").length } Minutes read </p>
+
+            <h2>Preview</h2>
+            <p>{text}</p>
+
+        </div>
+
+
+        </>
     )
 }
