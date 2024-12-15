817b0830cf14cec57f78e9245f1bbb8377456989
Hardik kumar <hardikgugnani123@gmail.com>
2024-12-15 02:36:41 -0500

    Done until Video 13

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,37 +1,50 @@
-import './App.css';
+import "./App.css";
 // import About from './components/About';
-import Navbar from './components/Navbar';
-import React, {useState} from 'react'
-import TextForm from './components/TextForm'
+import Navbar from "./components/Navbar";
+import React, { useState } from "react";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
 
 function App() {
+  const [mode, setMode] = useState("light"); // Whether darkMode is enabled or no
+  const [alert, setalert] = useState(null);
 
-  const [mode, setMode] = useState('light') // Whether darkMode is enabled or no
+  const showAlert = (message, type) => 
+    {
+      setalert({
+        msg: message,
+        type: type 
+      }),
+      setTimeout(() => {
+        setalert(null)
+      }, 1000);
 
-  const toggleMode = () =>{
-    if(mode === 'light'){
-      setMode('dark');
+    };
 
-      document.body.style.backgroundColor= 'grey'
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
 
-      }else{
-        setMode('light');
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
 
-        document.body.style.backgroundColor= 'white'
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
 
-      }
-  }
+    }
+  };
 
   return (
-    <>  
-<Navbar titttle = "TextUtils22"  mode={mode} toggleMode={toggleMode} />
-<div className='container my-3' >
-<TextForm  heading="Enter the text to analyze"  mode={mode}  />
+    <>
+      <Navbar titttle="TextUtils22" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <TextForm  showAlert={showAlert}  heading="Enter the text to analyze" mode={mode} />
 
-{/* <About/> */}
-
-
-</div>
+        {/* <About/> */}
+      </div>
     </>
   );
 }
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -7,6 +7,7 @@ export default function TextForm(props) {
 
 
          setText(newText);
+         props.showAlert("Text has been converted to UpperCase","success")
     }
 
     const handleLoClick = () =>{
@@ -14,15 +15,30 @@ export default function TextForm(props) {
 
 
          setText(newText);
+         props.showAlert("Text has been converted to LowerCase","success")
+
     }
 
     const handleOnChange = (event) =>{
         setText(event.target.value)
-        // setText("You just triggered handleUpClick")
+
+        
     }
 
     const RemoveText = () =>{
         setText("");
+        props.showAlert("Text has been REMOVED","info")
+
+    }
+
+    const handleCopy = () =>{
+        console.log("I am copy");
+        var text = document.getElementById('myBox');
+        text.select();
+        text.setSelectionRange(0,9999);
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied to clipboard",'success')
+
 
     }
 
@@ -33,11 +49,12 @@ export default function TextForm(props) {
         <div style={{ color: props.mode === 'dark' ? 'white':'black' }}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8" onChange={handleOnChange}  value={text}  style={{ backgroundColor: props.mode === 'dark' ? 'grey':'white' }}    ></textarea>
+                <textarea className="form-control" id="myBox" rows="8" onChange={handleOnChange}  value={text}  style={{ backgroundColor: props.mode === 'dark' ? 'grey':'white', color: props.mode === 'dark' ? 'white' : 'black' }}    ></textarea>
             </div>
             <button className='btn btn-primary mx-2' onClick={handleUpClick} >Convert to Uppercase</button>
             <button className='btn btn-primary mx-2' onClick={handleLoClick} >Convert to Lowecase</button>
             <button className='btn btn-primary mx-2' onClick={RemoveText} >Remove all</button>
+            <button className='btn btn-primary mx-2' onClick={handleCopy} >Copy to Clipboard</button>
         </div>
 
         <div className='container my-8' style={{ color: props.mode === 'dark' ? 'white':'black' }}>
@@ -46,7 +63,7 @@ export default function TextForm(props) {
             <p> Its a {0.008 * text.split(" ").length } Minutes read </p>
 
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length >0? text: "Enter something to preview it here"  }</p>
 
         </div>
 
