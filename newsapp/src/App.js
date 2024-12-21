390a90ee1c4b8086660e4ff194aa92f9fc75449b
Hardik kumar <125508104+HardikKumariscoding@users.noreply.github.com>
2024-12-21 02:10:22 -0500

    Video 23: structure of our NewsMonkey app

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 // rcc for class based component
 export default class App extends Component {
    c = "John"
   render() {
     return (
-      <div>Hello my first class based component {this.c}</div>
+      <div>
+        <Navbar/>
+        <News/>
+      </div>
     )
   }
 }
